237cf7eb9bd2da3f3a5fbc614a7eb462b21c2916
F1r3Site <f1r3bl4ze@gmail.com>
2018-01-31 23:10:31 -0500

    Add files via upload

--- ScienceProject.js
+++ ScienceProject.js
@@ -1,20 +1,15 @@
 const MASHAPE_API_KEY = "31PIkspQlQmshKrLYuu7DCRdQc0dp1uecH3jsnjvOjGiSlKwHJ";
 const MASHAPE_API_HOST = "wordsapiv1.p.mashape.com";
-const YOUR_API_KEY = "66705a0c886e45269259d802b030a51b";
-const YOUR_API_SECRET = "ca4c6d12c1d94243aa9a068501a9ce39";
+
+const NOUN_PROJECT_API_KEY = "66705a0c886e45269259d802b030a51b";
+const NOUN_PROJECT_API_SECRET = "ca4c6d12c1d94243aa9a068501a9ce39";
 
 var chosenWords = [];
 
 $(function() {
     //var canvas = $("#canvas");
     //var ctx = canvas[0].getContext("2d");
-
     $("#enter").click(start);
-    // $("#view").click(function() {
-    //     chosenWords.forEach(c => {
-    //         $("#debug").append(JSON.stringify(c)+"\n");
-    //     });
-    // });
 });
 
 function start() {
@@ -22,12 +17,9 @@ function start() {
     if (!input) {
         return;
     }
-
     resetInterface();
 
     var words = input.trim().split(" ");
-    chosenWords = [];
-
     var promises = [];
 
     words.forEach(w => {
@@ -39,21 +31,72 @@ function start() {
 }
 
 function processChosenWords() {
-    console.log("Finished with all!");
+    log("Finished with all!");
     chosenWords.forEach(w => {
         var wordDetail = w.words[w.selectedIndex];
-        console.log("checking "+wordDetail.partOfSpeech);
+        log("checking "+wordDetail.partOfSpeech);
         if (wordDetail.partOfSpeech == "noun") {
-            console.log("Found the noun: " + wordDetail.word);
-            $.ajax({
-                url: "http://api.thenounproject.com/icon/"+wordDetail.word,
-                headers: {
-                    "oauth_consumer_key": YOUR_API_KEY, 
-                    "oauth_consumer_secret": YOUR_API_SECRET
-                }
-            })
+            log("Found the noun: " + wordDetail.word);
+
+            findImageForNoun(wordDetail.word);
         }
     });
+}
+
+function findImageForNoun(noun) {
+    // Initialize
+    const oauth = OAuth({
+        consumer: {
+            key: NOUN_PROJECT_API_KEY,
+            secret: NOUN_PROJECT_API_SECRET
+        },
+        signature_method: 'HMAC-SHA1',
+        hash_function(base_string, key) {
+            return CryptoJS.HmacSHA1(base_string, key).toString(CryptoJS.enc.Base64);
+        }
+    });
+    
+    const request_data = {
+        url: "http://api.thenounproject.com/icon/"+noun,
+        method: "GET",
+    };
+    
+    // // Note: The token is optional for some requests
+    // const token = {
+    //     key: '370773112-GmHxMAgYyLbNEtIKZeRNFsMKPR9EyMZeS9weJAEb',
+    //     secret: 'LswwdoUaIvS8ltyTt5jkRh4J50vUPVVHtR2YPi5kE'
+    // };
+
+    var auth = oauth.authorize(request_data);
+
+    var h = oauth.toHeader(auth);
+
+    log("1: "+JSON.stringify(auth));
+    log("2: "+JSON.stringify(h));
+
+    /*
+    Authorization: OAuth oauth_consumer_key="66705a0c886e45269259d802b030a51b", oauth_nonce="fd1vd7qZRAMQBnoCHljbf3rgu8K429hO", oauth_signature="ndI71Zhrd5gbSYRSw0v85J%2BGmmM%3D", oauth_signature_method="HMAC-SHA1", oauth_timestamp="1517455338", oauth_version="1.0"
+    Authorization: OAuth oauth_consumer_key=\"66705a0c886e45269259d802b030a51b\", oauth_nonce=\"D7Hh7w1PEMUXDijIiHED6wTiQArMRh6q\", oauth_signature=\"deca60cc1fa4871727a3e1cde4caebaa0d355ff2\", oauth_signature_method=\"HMAC-SHA1\", oauth_timestamp=\"1517455506\", oauth_version=\"1.0\""
+    */
+
+    $.ajax({
+        url: request_data.url,
+        type: request_data.method,
+        contentType: 'x-www-form-urlencoded',
+        dataType: 'json',
+        headers: h,
+    }).always(function(data) {
+        log(JSON.stringify(data))
+    });
+    
+    // $.ajax({
+    //     url: request_data.url,
+    //     type: request_data.method,
+    //     //data: request_data.data,
+    //     headers: oauth.toHeader(oauth.authorize(request_data))
+    // }).always(function(data) {
+    //     log(JSON.stringify(data))
+    // });
 }
 
 function processWord(w) {
@@ -71,7 +114,7 @@ function processWord(w) {
                     cWord.addWord(w, result.definition, result.partOfSpeech, result.synonyms, result.typeOf);
                 });
             }
-            console.log("Finished with '"+w+"'.");
+            log("Finished with '"+w+"'.");
         }).fail(function(xhr, textStatus, error) {
             showError("Something went wrong: '"+textStatus+",' Error: "+error);
         });
@@ -79,6 +122,7 @@ function processWord(w) {
 }
 
 function resetInterface() {
+    chosenWords = [];    
     $("#debug").html(null);
     $("#options").html(null);
     $("#errors").removeClass().html(null);
@@ -86,4 +130,8 @@ function resetInterface() {
 
 function showError(message) {
     $("#errors").hide().addClass("alert alert-danger").html(message).fadeIn("slow");
+}
+
+function log(message) {
+    console.log(message);
 }
